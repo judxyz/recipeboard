1f24fcadffe6cd2d7982f4cb61581937805b6a1d
“Judy <j.zhu6@share.epsb.ca>
2025-09-30 18:48:28 -0600

    changes for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+
+const URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: URL,
 })
+
+// no localhost in production, make url dynamic
 
 export default api;
